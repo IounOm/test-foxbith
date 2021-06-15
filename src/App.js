30ff8f1d49fc504d6ca97cb8626de906a4c57587
IounOm <iounnirach15@gmail.com>
2021-06-15 19:53:53 +0700

    change css  & update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import { Button, Box , container, styled, spacing, RadioGroup, FormControlLabel, Typography, Radio} from '@material-ui/core';
+import { Button, Box, RadioGroup, FormControlLabel, Typography, Radio} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+// import { ClassSharp } from '@material-ui/icons';
 
 
 function App() {
 
-  // const MyTextField = styled(TextField)({
-  //   width: '100%',
-  // });
-
   const useStyles = makeStyles({
+    body: {
+      backgroundColor: '#eeeeee',
+      fontFamily: 'Prompt',
+      fontStyle: 'normal',
+    },
     divHeader: {
       width: '100%',
       padding: '16px 0 16px 24px',
@@ -44,13 +49,22 @@ function App() {
       fontSize: '20px',
     },
     p: {
+      // fontSize: '14px',
+      // fontWeight: '500',
+      // color: '#FF5C00',
+      fontStyle: 'normal',
+      fontWeight: '500',
       fontSize: '14px',
-      fontWeight: 'bold',
+      lineHeight: '22px',
       color: '#FF5C00',
     },
     p1: {
+      // fontSize: '14px',
+      // fontWeight: '500',
+      fontStyle: 'normal',
+      fontWeight: '500',
       fontSize: '14px',
-      fontWeight: 'bold',
+      lineHeight: '22px',
       color: '#00040C',
     },
     btnAddQuestion: {
@@ -59,111 +73,271 @@ function App() {
       border: "1px solid #FF5C00",
       width:"100%"
     },
+    elementCursor: {
+      cursor: "pointer",
+    },
   });
   const classes = useStyles();
 
   // for radio change
-  const [selectedValue, setSelectedValue] = React.useState('1');
+  const [selectedValue, setSelectedValue] = useState('1');
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
     console.log(selectedValue);
   };
-
   const checkRadio = selectedValue;
+
+  // const questionAllBlog = ['1','2'];
+  // const [AddQuestionBlog, setAddQuestionBlog] = useState(questionAllBlog);
+  // function handleAdd() {
+  //   const newQuestion = AddQuestionBlog.concat(1);
+  //   setList(newList);
+  // }
+  const [inputQuestion, setInputQuestion] = useState([1]);
+  const handleChangeClick = () => {
+    const array = [...inputQuestion];
+    array.push(array.length+1);
+    setInputQuestion(array);
+    // setInputQuestion([setInputQuestion.length]);
+    console.log(array);
+  };
+  const handleChangeDelete = () => {
+    const array = [...inputQuestion];
+    array.pop();
+    setInputQuestion(array);
+  }
+  // const [questionAllBlog, setQuestionAllBlog] = useState('');
 
   return (
     <div>
       <container>
-        <from>
+        <form>
           {/* header */}
-          <Box component="div"className={classes.divHeader}>
-            <Box component="h5" fontSize="24px" display="inline" m={1}>🦊 Foxbith Questionnaire</Box>
+          <Box className={classes.divHeader}>
+            <Box 
+            component="h5" 
+            fontSize="24px" 
+            display="inline" 
+            m={1}>
+              🦊 Foxbith Questionnaire
+            </Box>
           </Box>
           
           {/* btnSave */}
-          <Box component="div" className={classes.divLine}></Box>
-            <Box component="div" className={classes.bgSave} display="flex" flexDirection="row-reverse" pt={1.5} pb={1.5} pr={3}>
-              <Button type="submit" variant="contained" className={classes.btnSave}>SAVE</Button>
-              <Button variant="contained" className={classes.btnCancel}>CANCEL</Button>
+          <Box className={classes.divLine}></Box>
+            <Box 
+            className={classes.bgSave} 
+            display="flex" 
+            flexDirection="row-reverse" 
+            pt={1.5} pb={1.5} pr={3}>
+              <Button 
+              type="submit" 
+              variant="contained" 
+              className={classes.btnSave}>
+                SAVE
+              </Button>
+              <Button 
+              variant="contained" 
+              className={classes.btnCancel}>
+                CANCEL
+              </Button>
             </Box>
-          <Box component="div" className={classes.divLine}></Box>
+          <Box className={classes.divLine}></Box>
 
           {/* question detail */}
-            <Box component="div" display="block" p={3} mt={3} ml={3} mr={3} bgcolor="#FFFFFF">
-              <Box component="h6" className={classes.h6}>Questionnaire Detail</Box>
-              <TextField className={classes.TextField} id="outlined-basic" label="Name" variant="outlined" required/>
+            <Box 
+            p={3} 
+            mt={3} ml={3} mr={3} 
+            bgcolor="#FFFFFF">
+              <Box 
+              component="h6" 
+              className={classes.h6}>
+                Questionnaire Detail
+              </Box>
+              <TextField 
+              className={classes.TextField} 
+              label="Name" 
+              variant="outlined" 
+              required/>
             </Box>
-            <Box component="div" display="block" ml={3} mr={3} bgcolor="#FFFFFF">
-              <Box component="div" className={classes.divLine}></Box>
-            </Box>
-
-          {/* question */}
-            <Box component="div" display="block" ml={3} mr={3} bgcolor="#FFFFFF">
-              <Box component="div" display="block" pt={3} pl={3} pr={3}>
-                <Box component="div" display="block">
-                  <Box component="h6" className={classes.h6}>Question 1</Box>
-                  <TextField className={classes.TextField} id="outlined-basic" label="Question" variant="outlined" required/>
-                </Box>
-
-                <Box component="div" display="flex" flexDirection="row" alignItems="center" marginTop="24px">
-                  <Radio checked={selectedValue === '1'} value='1' color="primary" name="radio-question" onChange={handleChange}/>
-                  {checkRadio == 1 ? <TextField className={classes.TextField} id="outlined-basic" label="Description" variant="outlined" helperText="The answer is correct" required/>
-                  : <TextField className={classes.TextField} id="outlined-basic" label="Description" variant="outlined" required/>}
-                  <svg width="50" height="18" viewBox="0 0 14 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1 16C1 17.1 1.9 18 3 18H11C12.1 18 13 17.1 13 16V4H1V16ZM3 6H11V16H3V6ZM10.5 1L9.5 0H4.5L3.5 1H0V3H14V1H10.5Z" fill="#00040C"/>
-                  </svg>
-                </Box>
-                <Box component="div" display="flex" flexDirection="row" alignItems="center" marginTop="24px">
-                  <Radio checked={selectedValue === '2'} value='2' color="primary" name="radio-question" onChange={handleChange}/>
-                  {checkRadio == 2 ? <TextField className={classes.TextField} id="outlined-basic" label="Description" variant="outlined" helperText="The answer is correct" required/>
-                  : <TextField className={classes.TextField} id="outlined-basic" label="Description" variant="outlined" required/>}
-                  <svg width="50" height="18" viewBox="0 0 14 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1 16C1 17.1 1.9 18 3 18H11C12.1 18 13 17.1 13 16V4H1V16ZM3 6H11V16H3V6ZM10.5 1L9.5 0H4.5L3.5 1H0V3H14V1H10.5Z" fill="#00040C"/>
-                  </svg>
-                </Box>
-              </Box>
-
-              {/* add choice */}
-              <Box component="div" display="flex" flexDirection="row" alignItems="center" pl={3} pr={3} bgcolor="#FFFFFF">
-              <svg width="45" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="#FF5C00"/>
-              </svg>
-              <Box component="p" className={classes.p}>ADD CHOICE</Box>
-              </Box>
-
-              {/* duplicate and delete */}
-              <Box component="div" display="flex" alignItems="center" pl={3} pr={3}>
-                <div className={classes.divLine}></div>
-              </Box>
-              
-              <Box component="div" display="flex" alignItems="center" pl={3} pr={3} bgcolor="#FFFFFF">
-                <Box component="div" display="flex" alignItems="center">
-                  <svg width="45" height="22" viewBox="0 0 19 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14 0H2C0.9 0 0 0.9 0 2V16H2V2H14V0ZM17 4H6C4.9 4 4 4.9 4 6V20C4 21.1 4.9 22 6 22H17C18.1 22 19 21.1 19 20V6C19 4.9 18.1 4 17 4ZM17 20H6V6H17V20Z" fill="#00040C"/>
-                  </svg>
-                  <p className={classes.p1}>DUPLICATE</p>
-                </Box>
-
-                <Box component="div" display="flex" alignItems="center">
-                  <svg width="45" height="18" viewBox="0 0 14 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1 16C1 17.1 1.9 18 3 18H11C12.1 18 13 17.1 13 16V4H1V16ZM3 6H11V16H3V6ZM10.5 1L9.5 0H4.5L3.5 1H0V3H14V1H10.5Z" fill="#00040C"/>
-                  </svg>
-                  <p className={classes.p1}>DELETE</p>
-                </Box>
-              </Box>
-              <Box component="div" display="block" bgcolor="#FFFFFF">
-                <Box component="div" className={classes.divLine}></Box>
+            <Box 
+            ml={3} mr={3} 
+            bgcolor="#FFFFFF">
+              <Box 
+              component="div" 
+              className={classes.divLine}>
               </Box>
             </Box>
+
+            {inputQuestion.map((x, index) => (
+              <Box 
+              ml={3} mr={3} 
+              bgcolor="#FFFFFF">
+                <Box 
+                pt={3} pl={3} pr={3}>
+                  <Box>
+                    <Box 
+                    component="h6"
+                    className={classes.h6}>
+                      Question {index+1}
+                      </Box>
+                    <TextField 
+                    className="TextField" 
+                    label="Question" 
+                    variant="outlined" 
+                    required/>
+                  </Box>
+
+                  <Box 
+                  display="flex" 
+                  flexDirection="row" 
+                  alignItems="center" 
+                  mt={3}>
+                    <Radio 
+                    checked={selectedValue === '1'} 
+                    value='1' 
+                    color="primary" 
+                    name="radio-question" 
+                    onChange={handleChange}/>
+                    {checkRadio == 1 ? 
+                    <TextField 
+                    className={classes.TextField} 
+                    label="Description" 
+                    variant="outlined" 
+                    helperText="The answer is correct" 
+                    required/>
+                    : 
+                    <TextField 
+                    className={classes.TextField}
+                    label="Description" 
+                    variant="outlined" 
+                    required/>}
+                    <DeleteOutlineIcon style={{marginLeft: '24px'}}/>
+                  </Box>
+                  <Box 
+                  display="flex" 
+                  flexDirection="row" 
+                  alignItems="center" 
+                  mt={3}>
+                    <Radio 
+                    checked={selectedValue === '2'} 
+                    value='2'
+                    color="primary" 
+                    name="radio-question" 
+                    onChange={handleChange}/>
+                    {checkRadio == 2 ? 
+                    <TextField 
+                    className={classes.TextField} 
+                    label="Description" 
+                    variant="outlined" 
+                    helperText="The answer is correct" 
+                    required/>
+                    : 
+                    <TextField 
+                    className={classes.TextField} 
+                    label="Description" 
+                    variant="outlined" 
+                    required/>}
+                    <DeleteOutlineIcon style={{marginLeft: '24px'}}/>
+                  </Box>
+                </Box>
+
+                {/* add choice */}
+                <Box 
+                display="flex" 
+                alignItems="center" 
+                p={3}
+                bgcolor="#FFFFFF">
+                  <Box 
+                  display="flex"
+                  alignItems="center"
+                  className={classes.elementCursor}>
+                    <AddIcon style={{ color: '#FF5C00' }} />
+                    <Typography
+                    className={classes.p}>
+                      ADD CHOICE
+                    </Typography>
+                  </Box>
+                </Box>
+
+                {/* duplicate and delete */}
+                <Box 
+                component="div" 
+                display="flex" 
+                alignItems="center" 
+                pl={3} pr={3}>
+
+                  <Box 
+                  component="div" 
+                  className={classes.divLine}>
+                  </Box>
+                </Box>
+                
+                <Box
+                display="flex" 
+                alignItems="center" 
+                p={3} 
+                bgcolor="#FFFFFF">
+                  <Box
+                  display="flex" 
+                  alignItems="center"
+                  mr={3.5}
+                  className={classes.elementCursor}>
+                    <FileCopyOutlinedIcon />
+                    <Typography 
+                    className={classes.p1}>
+                      DUPLICATE
+                    </Typography>
+                  </Box>
+
+                  <Box
+                  display="flex" 
+                  alignItems="center"
+                  className={classes.elementCursor}
+                  onClick={()=> {
+                    // const array = [...inputQuestion]
+                    // const Selectindex = inputQuestion.indexOf(index)
+                    // array.splice(Selectindex, 1)
+                    // setInputQuestion(array)
+
+                    const array = [...inputQuestion];
+                    array.pop(index);
+                    setInputQuestion(array);
+                  }
+                    
+                  }>
+                    <DeleteOutlineIcon />
+                    <Typography 
+                    className={classes.p1}>
+                      DELETE
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box 
+                bgcolor="#FFFFFF">
+                  <Box 
+                  className={classes.divLine}>
+                  </Box>
+                </Box>
+              </Box>
+            ))}
 
             {/* Add question */}
-              <Box component="div" display="flex" alignItems="center" p={3} ml={3} mr={3} bgcolor="#FFFFFF">
-                <Button variant="contained" className={classes.btnAddQuestion}>
-                <svg width="45" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="#FF5C00"/>
-                </svg>ADD QUESTION</Button>
+              <Box 
+              display="flex" 
+              alignItems="center" 
+              p={3} 
+              ml={3} mr={3} 
+              bgcolor="#FFFFFF">
+                <Button 
+                variant="contained" 
+                className={classes.btnAddQuestion}
+                onClick={handleChangeClick}>
+                  <AddIcon style={{ color: '#FF5C00' }}/>
+                  ADD QUESTION
+                </Button>
               </Box>
-        </from>
+        </form>
       </container>
     </div>
   );
